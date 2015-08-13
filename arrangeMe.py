6427822d805eea8dcea7997d7e47914d73dbab45
ChungChe <r94943159@ntu.edu.tw>
2015-08-13 13:10:40 +0000

    Add a utility to move Apen
    	new file:   arrangeMe.py

---
+++ arrangeMe.py
@@ -0,0 +1,24 @@
+# Auto Apen move
+# Manual naming Apen to product ID
+# 
+# Example:
+#   Apen File Name: SNIS-999
+#   It will be moved to /somepath/SNIS
+import glob, os
+import re
+os.chdir("/nas")
+for file1 in glob.glob("*"):
+    # Exact match
+    match = re.match(r'^([A-Z]+)-\d+.*$', file1)
+    if match != None: 
+        print('file: '+ file1)
+        for prefix in match.groups():
+            print(prefix)
+            # moving this file1 to correct place
+            # 1. if the folder is not exists, create it
+            path = '/somepath/' + prefix
+            if not os.path.exists(path):
+                os.makedirs(path)
+            # 2. move
+            print('move ' + file1 + ' to ' + path + '/' + file1)
+            os.rename(file1, path + '/' + file1) 
