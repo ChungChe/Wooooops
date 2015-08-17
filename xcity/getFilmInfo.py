0973f6982f987705a80e5abeb13f8241819bc6ff
ChungChe <r94943159@ntu.edu.tw>
2015-08-17 14:37:42 +0000

    modified:   getFilmInfo.py

--- xcity/getFilmInfo.py
+++ xcity/getFilmInfo.py
@@ -17,63 +17,74 @@ import download as dl
 import toSqlite as w
 os.chdir("video");
 def get_filmInfo(soup, av_ID):
-#    title = soup.find('span', {"id": "program_detail_title"})
-#    if title == None:
-#        print("Title is empty, skip")
-#        return None
-#    film_name = title.string
-#    print(film_name)
-#    content_section = soup.find('div', {"class": "content"})
-#    if content_section == None:
-#        return None
-#        
-#    av_name = None
-#    av_name_section = content_section.find('li', {"id": "program_detail_credit"})
-#    if av_name_section != None:
-#        av_name = av_name_section.find('a').string
-#        print(av_name)
-#
-#    company = None
-#    company_span = content_section.find('span', {"id": "program_detail_label_name"})
-#    if company_span != None:
-#        company = company_span.string
-#        print(company)
-#
-#    release_date = None
-#    release_date_span = content_section.find('span', text=re.compile(u'発売日'))
-#    if release_date_span != None:
-#        if len(release_date_span.parent.contents) == 2:
-#            release_date = release_date_span.parent.contents[1].strip().replace('/','-')
-#            print(release_date)
-#    tag_section = content_section.find('span', text=re.compile(u'ジャンル'))
-#    if tag_section != None:
-#        tags = tag_section.parent.find_all('a')
-#        for tag in tags:
-#            print(tag.string.strip())
-#    product_id = None
-#    product_prefix = None
-#    product_id_span = content_section.find('span', text=re.compile(u'品番'))
-#    if product_id_span != None:
-#        if len(product_id_span.parent.contents) == 3:
-#            product_id = product_id_span.parent.contents[2].strip()
-#            match = re.match(r'^([a-z][A-Z]+).*$', product_id)
-#            real_product_id = None
-#            if match == None:
-#                real_product_id = product_id
-#            else:
-#                for prefix in match.groups():
-#                    real_product_id = product_id.replace(prefix, prefix+'-')
-#                    product_prefix = prefix
-#            print(real_product_id)
-    # photo
+    title = soup.find('span', {"id": "program_detail_title"})
+    if title == None:
+        print("Title is empty, skip")
+        return None
+    film_name = title.string
+    print(film_name)
+    content_section = soup.find('div', {"class": "content"})
+    if content_section == None:
+        return None
+        
+    av_name = None
+    av_name_section = content_section.find('li', {"id": "program_detail_credit"})
+    if av_name_section != None:
+        av_name = av_name_section.find('a').string
+        print(av_name)
+
+    company = None
+    company_span = content_section.find('span', {"id": "program_detail_label_name"})
+    if company_span != None:
+        company = company_span.string
+        print(company)
+
+    release_date = None
+    release_date_span = content_section.find('span', text=re.compile(u'発売日'))
+    if release_date_span != None:
+        if len(release_date_span.parent.contents) == 2:
+            release_date = release_date_span.parent.contents[1].strip().replace('/','-')
+            print(release_date)
+    product_id = None
+    product_prefix = None
+    product_id_span = content_section.find('span', text=re.compile(u'品番'))
+    if product_id_span != None:
+        if len(product_id_span.parent.contents) == 3:
+            product_id = product_id_span.parent.contents[2].strip()
+            match = re.match(r'^([a-z][A-Z]+).*$', product_id)
+            real_product_id = None
+            if match == None:
+                real_product_id = product_id
+            else:
+                for prefix in match.groups():
+                    real_product_id = product_id.replace(prefix, prefix+'-')
+                    product_prefix = prefix
+            print(real_product_id)
+    
+    tag_section = content_section.find('span', text=re.compile(u'ジャンル'))
+    if tag_section != None:
+        tags = tag_section.parent.find_all('a')
+        for tag in tags:
+            print(tag.string.strip()),
+        print('\n');
+#    return [av_ID+10000, av_name, birthdate, birthplace, height, bloodType, measurement]
+
+
+
+# get photo
+def getPhoto(soup):
     photo_section = soup.find('div', {"class": "photo"})
     if photo_section != None:
         tn_section = photo_section.find('p', {"class": "tn"})
         if tn_section != None:
+            # May have no photo
+            if tn_section.find('a') == None: 
+                return None
             url = tn_section.a.img['src'].split('?width')[0]
+            if 'noimage.gif' in url:
+                return None
             download_path = "../video_img/" + str(av_ID) + ".jpg"
             dl.download_url(url, download_path)
-#    return [av_ID+10000, av_name, birthdate, birthplace, height, bloodType, measurement]
 
 try:
     con = db.connect('../AV.db')
@@ -97,7 +108,7 @@ try:
 #w.updateResultToDB(cur, result)
         f.close()
 #con.commit()
-        os.rename(file, '../proceed_video/' + file)
+#    os.rename(file, '../proceed_video/' + file)
 except db.Error, e:
     if con:
         con.rollback()
