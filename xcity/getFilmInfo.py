b13df83f36727919dcc1987e0c5a3ca4451d821a
ChungChe <r94943159@ntu.edu.tw>
2015-08-18 14:38:16 +0000

    Support writing data to DB
    	modified:   getFilmInfo.py
    	modified:   toSqlite.py

--- xcity/getFilmInfo.py
+++ xcity/getFilmInfo.py
@@ -16,13 +16,16 @@ import glob, os
 import download as dl
 import toSqlite as w
 os.chdir("video");
+# return [film_name, av_name, company, release_date, real_product_id, film_tags]
 def get_filmInfo(soup, av_ID):
+    
     title = soup.find('span', {"id": "program_detail_title"})
     if title == None:
         print("Title is empty, skip")
         return None
     film_name = title.string
     print(film_name)
+
     content_section = soup.find('div', {"class": "content"})
     if content_section == None:
         return None
@@ -45,14 +48,15 @@ def get_filmInfo(soup, av_ID):
         if len(release_date_span.parent.contents) == 2:
             release_date = release_date_span.parent.contents[1].strip().replace('/','-')
             print(release_date)
+    
     product_id = None
+    real_product_id = None
     product_prefix = None
     product_id_span = content_section.find('span', text=re.compile(u'品番'))
     if product_id_span != None:
         if len(product_id_span.parent.contents) == 3:
             product_id = product_id_span.parent.contents[2].strip()
-            match = re.match(r'^([a-z][A-Z]+).*$', product_id)
-            real_product_id = None
+            match = re.match(r'^([a-zA-Z]+).*$', product_id)
             if match == None:
                 real_product_id = product_id
             else:
@@ -60,13 +64,19 @@ def get_filmInfo(soup, av_ID):
                     real_product_id = product_id.replace(prefix, prefix+'-')
                     product_prefix = prefix
             print(real_product_id)
-    
+    film_tags = []
     tag_section = content_section.find('span', text=re.compile(u'ジャンル'))
     if tag_section != None:
         tags = tag_section.parent.find_all('a')
         for tag in tags:
-            print(tag.string.strip()),
+            tagString = tag.string.strip()
+            film_tags.append(tagString)
+            print(tagString),
         print('\n');
+    
+    return [film_name, av_name, company, release_date, real_product_id, film_tags]
+
+
 #    return [av_ID+10000, av_name, birthdate, birthplace, height, bloodType, measurement]
 
 
@@ -85,6 +95,8 @@ def getPhoto(soup):
                 return None
             download_path = "../video_img/" + str(av_ID) + ".jpg"
             dl.download_url(url, download_path)
+
+#w.create_tag_film_table('../AV.db')
 
 try:
     con = db.connect('../AV.db')
@@ -105,10 +117,10 @@ try:
 #            continue
         # update result to DB
 #        print(result)
-#w.updateResultToDB(cur, result)
+        w.updateResultToDB(cur, result)
         f.close()
-#con.commit()
-#    os.rename(file, '../proceed_video/' + file)
+        con.commit()
+        os.rename(file, '../proceed_video/' + file)
 except db.Error, e:
     if con:
         con.rollback()
--- xcity/toSqlite.py
+++ xcity/toSqlite.py
@@ -1,21 +1,106 @@
 import sqlite3 as db
 import sys
 
+def create_tag_film_table(dbName):
+    try:
+        con = db.connect(dbName)
+        cur = con.cursor()
+        cur.execute("create table tag_filmId ( \
+                        tag_id INTEGER REFERENCES tag(id), \
+                        film_id INTEGER REFERENCES film(film_ID) \
+                    )")
+        con.commit()
+    except db.Error, e:
+        if con:
+            con.rollback()
+        print("Error %s:" % e.args[0])
+        sys.exit(1)
+    finally:
+        if con:
+            con.close()
+        
+
+# 1. find film, if the film exists, update product_id, otherwise, create a new film
+# 2. get film ID from DB
+# 3. get actressID from DB
+# 4. if actressID in invalid, create a new actressID & actressIDInfo
+# 5. write info to actress_film
+# 6. handle tags
+#    find tag, if the tag is not exists, create a new tag
+#    update tag
+#
+# [film_name, av_name, company, release_date, real_product_id, film_tags]
+#     0         1         2          3                4            5
 def updateResultToDB(cur, result):
     if result == None:
         return
-    av_real_name = result[1]
-    cur.execute('select * from actress where real_name=:Name', {"Name": av_real_name})
-    matchAV = cur.fetchone()
-    if matchAV == None:
-        print(av_real_name + ' is not in the DB, create it')
-        actress_data = [result[0], result[1], result[0]]
-        print(actress_data)
-        insert_actress(cur, actress_data)
-        actressInfo_data = [result[0], result[2], result[3], result[4], result[5], result[6]]
-        insert_actressInfo(cur, actressInfo_data)
+    film_name = result[0]
+    av_name = result[1]
+    cur.execute('select * from film where title=:Title', {"Title": film_name})
+    matchFilm = cur.fetchone()
+    product_id = result[4]
+    # update product_id only
+    if matchFilm != None:
+        film_ID = result[0]
+        print('update film_ID ' + film_ID + ' with ' + product_id)
+        update_product_id(cur, product_id, film_ID)
+    # create new film data
     else:
-        print(av_real_name + 'is in the DB, no need to create')
+        company = result[2]
+        release_date = result[3]
+        film_data = [product_id, film_name, company, release_date]
+        insert_film_with_productId(cur, film_data)
+        print('insert new row ' + str(film_data))
+
+    cur.execute('select film_ID from film where title=:Title', {"Title": film_name})
+    matchFilmId = cur.fetchone()
+    filmID = None 
+    for fid in matchFilmId:
+        filmID = fid
+    avID = None
+    cur.execute('select av_ID from actress where real_name=:Name', {"Name": av_name})
+    print(av_name)
+    matchAVId = cur.fetchone()
+    # see video 35512, skip
+    if matchAVId == None:
+        print('Cannot find av_name in DB, skip...')
+        return None
+    for aid in matchAVId:
+        avID = aid
+    if avID != None and filmID != None:
+        actress_film_data = [aid, fid]
+        insert_actress_film(cur, actress_film_data)
+    # handle tags
+    filmTags = result[5]
+    for filmTag in filmTags:
+        #print(filmTag)
+        insert_tag(cur, [filmTag])
+        cur.execute('select id from tag where name=:Name', {"Name": filmTag})
+        tagList = cur.fetchone()
+        for tagItem in tagList:
+            if filmID == None:
+                continue
+            tag_filmId_data = [tagItem, filmID]
+#print(tag_filmId_data)
+            insert_tag_filmId(cur, tag_filmId_data) 
+        
+#    av_real_name = result[1]
+#    cur.execute('select * from actress where real_name=:Name', {"Name": av_real_name})
+#    matchAV = cur.fetchone()
+#    if matchAV == None:
+#        print(av_real_name + ' is not in the DB, create it')
+#        actress_data = [result[0], result[1], result[0]]
+#        print(actress_data)
+#        insert_actress(cur, actress_data)
+#        actressInfo_data = [result[0], result[2], result[3], result[4], result[5], result[6]]
+#        insert_actressInfo(cur, actressInfo_data)
+#    else:
+#        print(av_real_name + 'is in the DB, no need to create')
+
+def update_product_id(cur, product_id, film_ID):
+    cur.execute('update film set product_id=? where film_ID=?', (product_id, film_ID))
+def insert_film_with_productId(cur, data):
+    cur.execute('insert or ignore into film (product_id, title, company, release_date) values (?,?,?,?)', data) 
 #  1. For actress
 def insert_actress(cur, data):
     cur.execute('insert or ignore into actress (av_ID, real_name, info_ID) values (?,?,?)', data) 
@@ -37,4 +122,5 @@ def insert_link_info(cur, data):
 #  7. For actress_film
 def insert_actress_film(cur, data):
     cur.execute('insert or ignore into actress_film values (?,?)', data) 
-
+def insert_tag_filmId(cur, data):
+    cur.execute('insert or ignore into tag_filmId values (?,?)', data) 
