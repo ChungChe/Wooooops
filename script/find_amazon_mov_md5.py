d1f3c71eebd97ab105a887fec1b847264dfd9eb1
ChungChe <r94943159@ntu.edu.tw>
2015-12-05 14:59:47 +0000

    new file:   find_amazon_mov_md5.py

---
+++ script/find_amazon_mov_md5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+#import sys
+
+con = lite.connect('r94943159@ntu.edu.tw.db')
+
+with con:    
+    cur = con.cursor()    
+    cur.execute("SELECT kind, md5, name FROM nodes")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[0] != "FILE":
+            continue
+        if row[1] == None:
+            continue
+        if ".avi" not in row[2]:
+            continue
+        print str(row[1]) + ' ' + row[2].encode('utf-8')
