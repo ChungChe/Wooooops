67895d77bedf875ba16d317d576734aa63de7682
ChungChe <r94943159@ntu.edu.tw>
2016-08-17 18:11:09 +0800

    new file:   auto_dl_pic.py 	new file:   download.py

---
+++ auto_dl_pic.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Scan folder Sorted/
+# if /A/ACD/ACD-001/ACD-001.jpg doesn't exists
+# download from internet
+
+import os
+import re
+import sys
+import ntpath
+import scandir
+import errno
+# Python 3
+import urllib.request
+from bs4 import BeautifulSoup
+import download as dl 
+def scan_jpg(path):
+    if not os.path.exists(path):
+        print('Path not exists: {}'.format(path))
+        return
+    for entry in scandir.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scan_jpg(entry.path)  # see below for Python 2.x
+        else:
+            if ".jpg" not in entry.path:
+                file_without_ext = os.path.splitext(entry.path)[0]
+                jpg_file = "{}.jpg".format(file_without_ext)
+                if not os.path.exists(jpg_file):
+                    bn = ntpath.basename(file_without_ext)
+                    #print("{} doesn't exist, downloading...".format(bn))
+                    hyper_link_path = 'https://www.javbus.com/{}'.format(bn)
+                    content = ""
+                    try:
+                        content = urllib.request.urlopen(hyper_link_path).read()
+                    except urllib.error.HTTPError as e:
+                        pass
+                    soup = BeautifulSoup(content)
+                    link = soup.find('a', {'class': 'bigImage'})
+                    if link == None:
+                        print('Cannot find jpg link from "{}", abort...'.format(bn))
+                        continue
+                    link_path = link['href']
+                    to_path = '/Volumes/wd2/new_cover/{}.jpg'.format(bn) 
+                    print("Download '{}' -> '{}".format(link_path, to_path))
+                    dl.download_url(link_path, to_path)
+
+def scan(path):
+    print("Try to scan folder: {}".format(path))
+    entries = scan_jpg(path)
+
+    for entry in entries:
+        print(entry.path)
+        #move_single_file(entry.path, to_path)
+
+if len(sys.argv) < 2:
+    print("Usage: auto_dl_pic folder")
+    sys.exit()
+
+scan(os.path.expanduser(sys.argv[1]))
---
+++ download.py
@@ -0,0 +1,16 @@
+#import urllib
+from urllib.request import Request, urlopen
+import codecs
+def download_url(url, path_to_save):
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+    req = Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
+    
+    try:
+        content = urlopen(req)
+    except urllib.error.HTTPError as e:
+        pass
+    
+    f = codecs.open(path_to_save, 'wb')
+    f.write(content.read())
+    f.close()
