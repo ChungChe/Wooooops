e8b2ca69f6f43625e18e52af6f5533e06f9765d7
ChungChe <r94943159@ntu.edu.tw>
2016-08-17 22:41:54 +0800

    modified:   auto_dl_pic.py

--- auto_dl_pic.py
+++ auto_dl_pic.py
@@ -21,13 +21,24 @@ def scan_jpg(path):
         if entry.is_dir(follow_symlinks=False):
             yield from scan_jpg(entry.path)  # see below for Python 2.x
         else:
+            #print(entry.path) 
             if ".jpg" not in entry.path:
                 file_without_ext = os.path.splitext(entry.path)[0]
                 jpg_file = "{}.jpg".format(file_without_ext)
                 if not os.path.exists(jpg_file):
                     bn = ntpath.basename(file_without_ext)
-                    #print("{} doesn't exist, downloading...".format(bn))
-                    hyper_link_path = 'https://www.javbus.com/{}'.format(bn)
+                    #print("checking {}".format(bn)) 
+                    match =  re.match(r'^([A-Z0-9]+-[A-Z]*\d+)\D*', bn)
+                    if match == None:
+                        print("{} cannot pass the reg exp, skip".format(bn))
+                        continue
+                    product_id = ""
+                    for gg in match.groups():
+                        product_id = gg
+                        #print("Check {}".format(product_id))
+                    
+                    #print("{} doesn't exist, downloading...".format(product_id))
+                    hyper_link_path = 'https://www.javbus.com/{}'.format(product_id)
                     content = ""
                     try:
                         content = u''.join(urllib.request.urlopen(hyper_link_path).read().decode('utf-8'))
@@ -38,10 +49,14 @@ def scan_jpg(path):
                     soup = BeautifulSoup(content)
                     link = soup.find('a', {'class': 'bigImage'})
                     if link == None:
-                        print('Cannot find jpg link from "{}", abort...'.format(bn))
+                        print('Cannot find jpg link from "{}", abort...'.format(product_id))
+                        debug = soup.find('div', {'class': 'bol-md-9'})
+                        if debug == None:
+                            continue
+                        print(debug)
                         continue
                     link_path = link['href']
-                    to_path = '/Volumes/wd2/new_cover/{}.jpg'.format(bn) 
+                    to_path = '/Volumes/wd2/new_cover/{}.jpg'.format(product_id) 
                     if (os.path.exists(to_path)):
                         continue
                     print("Download '{}' -> '{}".format(link_path, to_path))
