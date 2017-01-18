a3e8175c1729762960bfaf93b6a68d772cb51aa1
ChungChe <r94943159@ntu.edu.tw>
2017-01-18 23:28:53 +0800

    modified:   maria01_parse.py 	new file:   member88_parse.py

--- member88_parse.py
+++ member88_parse.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-with open('/Users/duntex/Wooooops/maria01.1.html') as f:
+with open('/Users/duntex/Wooooops/member88.html') as f:
     lines = f.readlines()
 print("""
 <html>
@@ -12,14 +12,18 @@ print("""
 lst = []
 
 for line in lines:
-    pic_url = line.split("><br><a")[0].split("<img src=")[1]
-    tmp = line.split("<a href=")[1]
-    rapid_link = tmp.split("\">")[0] + "\""
-    title = tmp.split("\">")[1].split("/</a>")[0]
-    prod_id = title.split(' ')[1].split(' ')[0]
-    d = {'id': prod_id, 'title': title[1:], 'img_url': pic_url, 'rapid_url': rapid_link}
-    lst.append(d)
-
+    #try:
+        pic_url = line.split("><br><a")[0].split("<img src=")[1]
+        tmp = line.split("<a href=")[1]
+        rapid_link = tmp.split("\">")[0] + "\""
+        title = tmp.split("\">")[1].split("/</a>")[0]
+        prod_id = title.split(' ')[0]
+        d = {'id': prod_id, 'title': title[1:], 'img_url': pic_url, 'rapid_url': rapid_link}
+        #print("Title: {}".format(title))
+        #print("PID: {}".format(prod_id))
+        lst.append(d)
+    #except Exception as e:
+    #    print(line)
 lst.sort()
 count = 0
 row_size = 6
